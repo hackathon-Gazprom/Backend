2240e6be2145388bc175fb99dfb323a1eb48f563
mxnoob <noobmx@gmail.com>
2024-08-07 14:21:33 +0300

    fix imports

--- src/backend/api/v1/projects/filters.py
+++ src/backend/api/v1/projects/filters.py
@@ -1,4 +1,4 @@
-from django_filters.rest_framework import FilterSet, CharFilter
+from django_filters.rest_framework import CharFilter, FilterSet
 
 from apps.projects.models import Member
 
@@ -7,12 +7,10 @@ class MemberFilter(FilterSet):
     position = CharFilter(
         field_name="user__profile__position",
         lookup_expr="iexact",
-        # source="user__profile__position",
     )
     city = CharFilter(
         field_name="user__profile__city",
         lookup_expr="iexact",
-        # source="user__profile__city",
     )
 
     class Meta:
